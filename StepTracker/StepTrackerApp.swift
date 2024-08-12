84fc3c7ead0538f3c23e8369e4b9112a2e9f7768
KcDev35 <kcornuelle@protonmail.com>
2024-08-12 14:05:58 -0700

    Created Xcode Project
    Initial step tracker project

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Kevin Cornuelle on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
